1c44ec8a7667ceea6ab77980822cb22bb30fe34d
Lastelitetiger <lastelitetiger@GMAIL.COM>
2023-02-28 10:15:51 -0800

    2
    movment still not working for some reason

--- Custom-Game-New/Assets/Scripts/PlayerMovement.cs
+++ Custom-Game-New/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Movement")]
+    public float moveSpeed;
+
+    public Transform orientation;
+
+     float horizontalInput;
+     float verticalInput;
+
+    private Vector3 moveDirection;
+
+    Rigidbody rb;
+
+    private void Start()
     {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+    
+    private void Update()
+    {
+        MyInput();
         
     }
-
-    // Update is called once per frame
-    void Update()
+    private void MyInput()
     {
-        
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+    private void FixedUptate()
+    {
+        MovePlayer();
+    }
+
+
+    private void MovePlayer()
+    {
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+
     }
 }
