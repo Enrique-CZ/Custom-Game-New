1022e47715487fedf2696b12b8dbd85bd387a555
mark146731 <mark.146731@nv.ccsd.net>
2023-03-15 00:35:26 -0700

    I hope this works

--- Custom-Game-New/Assets/Scripts/PickUpItem.cs
+++ Custom-Game-New/Assets/Scripts/PickUpItem.cs
@@ -4,24 +4,76 @@ using UnityEngine;
 
 public class PickUpItem : MonoBehaviour
 {
+    public GameObject Rock;
+    public GameObject PickUpText;
+    public GameObject DropText;
+    public Transform RockParent;
 
-    public GameObject box;
-
-    // Start is called before the first frame update
     void Start()
     {
-        box.SetActive(false);   
+        Rock.GetComponent<Rigidbody>().isKinematic = true;
+        PickUpText.SetActive(false);
+        DropText.SetActive(false);
     }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.F))
+        {
+            DropText.SetActive(false);
+            Drop();
+        }
+    }
+    void Drop()
+    {
+        RockParent.DetachChildren();
+        Rock.transform.eulerAngles = new Vector3(Rock.transform.position.x, Rock.transform.position.z, Rock.transform.position.y);
+        Rock.GetComponent<Rigidbody>().isKinematic = false;
+        Rock.GetComponent<MeshCollider>().enabled = true;
+    }
+    void EquipRock()
+    {
+        Rock.GetComponent<Rigidbody>().isKinematic = true;
+        Rock.transform.position = RockParent.transform.position;
+        Rock.transform.rotation = RockParent.transform.rotation;
 
+        Rock.GetComponent<MeshCollider>().enabled = false;
+
+        Rock.transform.SetParent(RockParent);
+
+    }
+    bool hasBeenUsed;
     private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if(hasBeenUsed == true)
+            {
+                PickUpText.SetActive(true);
+            }
+            else
+            {
+                PickUpText.SetActive(false);
+                DropText.SetActive(true);
+            }
+            if (Input.GetKey(KeyCode.E))
+            {
+                hasBeenUsed = false;
+                EquipRock();
+            }
+        }
+
+    }
+    private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
-            if(Input.GetKey(KeyCode.E))
-            {
-                this.gameObject.SetActive(false);
-                box.SetActive(true);
-            }
+            hasBeenUsed = true;
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        PickUpText.SetActive(false);
+        DropText.SetActive(false);
     }
 }
