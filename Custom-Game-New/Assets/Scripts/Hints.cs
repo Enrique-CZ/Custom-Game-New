f332e59543088fe5b96eca30ba79533a7df89920
mark146731 <mark.146731@nv.ccsd.net>
2023-03-24 08:53:01 -0700

    2

--- Custom-Game-New/Assets/Scripts/Hints.cs
+++ Custom-Game-New/Assets/Scripts/Hints.cs
@@ -3,21 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Hints : MonoBehaviour
-{ 
+{
 
     public GameObject HintFirstLevel;
-    private bool hasBeenUsedOnLevel;
-    
+    public GameObject MaxHints;
+    public GameObject keyImage1;
+    public GameObject keyImage2;
+    public GameObject keyImage3;
+
+
+
+    private bool hasBeenUsedOnLevel = false;
+
+    float startTime = 3f;
+    float currentTime = 0f;
+
+
 
     void Start()
     {
+        currentTime = startTime;
         HintFirstLevel.SetActive(false);
+        MaxHints.SetActive(false);
     }
     void Update()
     {
+        currentTime -= 1 * Time.deltaTime;
+        if (currentTime < 0)
+        {
+            HintFirstLevel.SetActive(false);
+            MaxHints.SetActive(false);
+        }
         if (Input.GetKeyDown(KeyCode.H))
         {
-            HintFirstLevel.SetActive(true);
+            if(hasBeenUsedOnLevel)
+            {
+                MaxHints.SetActive(true);
+                restart();
+            }
+            else
+            {
+                HintFirstLevel.SetActive(true);
+                hasBeenUsedOnLevel = true;
+                restart();
+                keyImage3.SetActive(false);
+            }
         }
+    }
+    public void restart()
+    {
+        currentTime = startTime;
     }
 }
